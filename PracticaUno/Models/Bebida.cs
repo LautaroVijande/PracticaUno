c7782ecfa5c9c37efdd800eb3ef2d15788c79fb7
LautaroVijande <lautarovijande@itisjt.edu.ar>
2024-04-23 21:05:30 -0300

    Agregar archivos de proyecto.

---
+++ PracticaUno/Models/Bebida.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaUno.Models
+{
+    internal class Bebida
+    {
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public Bebida(string nombre, int cantidad) //CONSTRUCTOR
+        {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+        } 
+
+        public void tomarse(int cuantoBebio)//CONSTRUCTOR
+        {
+            this.cantidad -= cuantoBebio;
+        }
+    }
+
+}
---
+++ PracticaUno/Program.cs
@@ -0,0 +1,20 @@
+﻿using PracticaUno.Models;
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+        Bebida bebida = new Bebida("CocaCola", 500);
+        Bebida bebida2 = new Bebida("Fanta", 500);
+        //bebida.nombre = "CocaCola";
+        //bebida.cantidad = 500;
+
+        bebida.tomarse(100); 
+        bebida2.tomarse(300);
+        Console.WriteLine("A la persona le queda " + bebida.cantidad + "cc de la bebida " + bebida.nombre);
+        Console.WriteLine("A la persona le queda " + bebida2.cantidad + "cc de la bebida " + bebida2.nombre);
+
+    }
+
+}
